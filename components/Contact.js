14b54d157b1be0ae2b0838ffe0f9cecd3e2c706c
Mateusz Kotwiński <mkotwinski98@gmail.com>
2019-07-26 10:48:12 +0200

    fix syntax

--- components/Contact.js
+++ components/Contact.js
@@ -1,6 +1,6 @@
 var Contact = React.createClass({
   propTypes: {
-    item: React.PropTypes.object.isRequired,
+    item: React.PropTypes.object.isRequired 
   },
 
   render: function() {
@@ -8,7 +8,7 @@ var Contact = React.createClass({
       <div className={'contactItem'}>
         <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
         <p className={'contactLabel'}>
-           Imię: {this.props.contact.firstName}
+           Imię: {this.props.item.firstName}
         </p>
         <a href={'mailto:' + this.props.item.email}>
           {this.props.item.email}
